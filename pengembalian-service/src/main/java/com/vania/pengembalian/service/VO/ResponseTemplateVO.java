80bf401b297cdcea8b2958a240f9f44576ddffee
VaniaVikriRamadhani <113493623+VaniaVikriRamadhani@users.noreply.github.com>
2023-01-27 18:48:02 +0700

    Add files via upload

---
+++ pengembalian-service/src/main/java/com/vania/pengembalian/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vania.pengembalian.service.VO;
+
+import com.vania.pengembalian.service.entity.Pengembalian;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author HP
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private Pengembalian pengembalian;
+    private Peminjaman peminjaman;
+}
